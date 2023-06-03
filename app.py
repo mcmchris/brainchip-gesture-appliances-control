e0365f004a21ff2b31de80cd24b31b4cb9709075
Christopher Mendez <mcmchris@hotmail.com>
2023-06-03 11:30:06 -0400

    conditions to send http post

--- app.py
+++ app.py
@@ -57,6 +57,13 @@ def gen_frames():  # generate frame by frame from camera
                         #print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
                         for label in labels:
                             score = res['result']['classification'][label]
+                            if label == "light" and score > 0.9:
+                                print("Creo que es light")
+                            if label == "ac" and score > 0.9:
+                                print("Creo que es ac")
+                            if label == "tv" and score > 0.9:
+                                print("Creo que es tv")
+
                             #print('%s: %.2f\t' % (label, score), end='')
                         #print('', flush=True)
 
@@ -75,13 +82,6 @@ def gen_frames():  # generate frame by frame from camera
                         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
                     next_frame = now() + 100
-
-                    if label == "light" and score > 0.9:
-                        print("Creo que es light")
-                    if label == "ac" and score > 0.9:
-                        print("Creo que es ac")
-                    if label == "tv" and score > 0.9:
-                        print("Creo que es tv")
                     
             finally:
                 if (runner):
