a515628883096ea174795df8ed9e61e6a85e13f5
Christopher Méndez <mcmchris@hotmail.com>
2023-12-23 11:09:14 -0400

    streaming patch

--- newcam.py
+++ newcam.py
@@ -140,5 +140,5 @@ def index():
     return render_template('index.html')
 
 
-#if __name__ == '__main__':
-app.run(host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
