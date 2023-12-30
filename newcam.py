d8f379477af28121e7a93835eba18ee6101d016b
Christopher Méndez <mcmchris@hotmail.com>
2023-12-30 09:31:23 -0400

    debug disabled

--- newcam.py
+++ newcam.py
@@ -146,7 +146,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=False)
 
 # Clean up
 #cv2.destroyAllWindows()
