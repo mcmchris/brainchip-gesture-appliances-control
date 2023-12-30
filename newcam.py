857ba20e18c6457bb6d7e90e38fcc68b413e79c9
Christopher Méndez <mcmchris@hotmail.com>
2023-12-30 09:22:39 -0400

    host ip

--- newcam.py
+++ newcam.py
@@ -138,7 +138,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
 
 # Clean up
 cv2.destroyAllWindows()
